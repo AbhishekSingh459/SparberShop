7c9fbbfb0674e8e648c52c6e9a97e70e08f6580e
Abhishek Singh <abhisheksingh.vns0709@gmail.com>
2024-12-28 21:03:26 +0530

    Item Card Removed

--- Sparber/src/main.jsx
+++ Sparber/src/main.jsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import NavbarComponent from './Component/NavbarComponent.jsx'
-import ItemCard from './Component/ItemCard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
     <NavbarComponent/>
-    <ItemCard />
   </StrictMode>,
 )
